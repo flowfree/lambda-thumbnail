ce20b5b62a5350f988f407dfa9c1607839e54b72
Nash <nashruddin.amin@gmail.com>
2021-06-21 15:51:26 +0000

    improve the code

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import mimetypes
 import os
 import sys
 import uuid
@@ -22,14 +23,22 @@ def resize_image(image_path, resized_path):
 def lambda_handler(event, context):
     for record in event['Records']:
         bucket = record['s3']['bucket']['name']
-        key = unquote_plus(record['s3']['object']['key'])
-        print(f'BUCKET = {bucket}, KEY = {key}')
-        tmpkey = key.replace('/', '')
+        key_src = unquote_plus(record['s3']['object']['key'])
+        key_dst = f'resized-{key_src}'
+        tmpkey = key_src.replace('/', '')
         download_path = '/tmp/{}{}'.format(uuid.uuid4(), tmpkey)
         upload_path = '/tmp/resized-{}'.format(tmpkey)
-        s3_client.download_file(bucket, key, download_path)
+        
+        s3_client.download_file(bucket, key_src, download_path)
         resize_image(download_path, upload_path)
-        s3_client.upload_file(upload_path, bucket, f'resized-{key}', ExtraArgs={'ContentType': 'image/jpeg', 'ACL': 'public-read'})
+        
+        mimetype, _ = mimetypes.guess_type(key_src)
+        args = {
+            'ContentType': mimetype,
+            'ACL': 'public-read'
+        }
+        s3_client.upload_file(upload_path, bucket, key_dst, ExtraArgs=args)
+        
     print('done.')
             
 
